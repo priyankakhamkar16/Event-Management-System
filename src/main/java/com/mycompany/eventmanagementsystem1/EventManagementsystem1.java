d71cefa04b20a3129388bd78f938e7ee00159f6e
Priyanka khamkar <118741221+priyankakhamkar16@users.noreply.github.com>
2023-06-08 14:24:13 +0530

    Files upload

---
+++ src/main/java/com/mycompany/eventmanagementsystem1/EventManagementsystem1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eventmanagementsystem1;
+
+/**
+ *
+ * @author priya
+ */
+public class EventManagementsystem1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
